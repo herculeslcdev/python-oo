177ea5e2c75936a95fb830a026527901829003c7
Hercules <herculeslcdev@gmail.com>
2020-02-17 20:58:59 -0300

    Entendendo Módulos

---
+++ main.py
@@ -0,0 +1,11 @@
+import calc
+
+calc.calculate(1, 1)
+calc.calculate_sub(1, 1)
+
+######################################
+
+from calc import calculate, calculate_sub
+
+calculate(1, 1)
+calculate_sub(1, 1)
