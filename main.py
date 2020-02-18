a9334d68044132bd8cc217420f90e8e7b9ecdc46
Hercules <herculeslcdev@gmail.com>
2020-02-17 21:13:56 -0300

    Criando nosso primeiro Package

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import calc
-
-calc.calculate(1, 1)
-calc.calculate_sub(1, 1)
-
-######################################
-
-from calc import calculate, calculate_sub
+from school_of_net.math.calc import calculate, calculate_sub
 
 calculate(1, 1)
 calculate_sub(1, 1)
+
+###################################
+
+from school_of_net.string.str import hello
+hello()
+
+###################################
+
+from school_of_net.sample import super
+
+super()
+
+
